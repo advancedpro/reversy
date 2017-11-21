9da86e22b67d4fd8c7b19673ea3b73882457e11e
dana <dana512@walla.co.il>
2017-11-21 16:03:12 +0200

    start of project

---
+++ Debug/Game.d
@@ -0,0 +1,16 @@
+Game.d: ../Game.cpp ../Game.h ../IPlayer.h ../Board.h ../Point.h \
+ ../PlayerTypes.h ../IRules.h ../IDisplayer.h
+
+../Game.h:
+
+../IPlayer.h:
+
+../Board.h:
+
+../Point.h:
+
+../PlayerTypes.h:
+
+../IRules.h:
+
+../IDisplayer.h:
